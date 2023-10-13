daa1f2bbd3cca98b1179c13ca7f0636865c0e2f6
Daniel Cabrera <danielcabrera1494@gmail.com>
2023-10-13 14:29:05 -0500

    updates

--- Disfluency detection/scripts/helper_functions.py
+++ Disfluency detection/scripts/helper_functions.py
@@ -230,4 +230,5 @@ def generate_speech(file_path, language, gpu=True):
     tts.tts_to_file(text="It took me quite a long time to develop a voice, and now that I have it I'm not going to be silent.",
                     file_path=file_path,
                     speaker_wav="/path/to/target/speaker.wav",
-                    language=language)
+                    language=language,
+    )
---
+++ Disfluency detection/scripts/prueba_xtts.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 13 14:19:49 2023
+
+@author: Daniel
+"""
+
