8b747ffd0126474197bdd0458c5cd571291dd731
Daniel Cabrera <danielcabrera1494@gmail.com>
2023-12-14 18:58:19 -0500

    Update classify v2.py

--- Disfluency detection/scripts/classify v2.py
+++ Disfluency detection/scripts/classify v2.py
@@ -9,6 +9,7 @@ import matplotlib
 matplotlib.use('Agg')  # Use a non-interactive backend
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 def get_device() :
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -115,27 +116,26 @@ def conv_audio_data(filename, device, bundle):
     emission = emission.cpu().detach().numpy()
     return emission
 
-def process_directory(directory, model, device, bundle, true_labels):
+def process_directory(all_audio_files, model, device, bundle, true_labels):
     results = []
     predictions = []
-    for root, dirs, files in os.walk(directory):
-        for filename in files:
-            if filename.lower().endswith('.wav'):
-                file_path = os.path.join(root, filename)
-                print(f"Processing {file_path}")
 
-                # Convert audio to feature encodings
-                audio_features = conv_audio_data(file_path, device, bundle)
+    for file_path in all_audio_files:
+        filename = os.path.basename(file_path)
+        print(f"Processing {file_path}")
 
-                # Make predictions
-                with torch.no_grad():
-                    print("predicting")
-                    outputs = model(torch.tensor(audio_features).unsqueeze(0).to(device))
-                    _, predicted = torch.max(outputs.data, 1)
-                    print(f"Predicted for {filename}: {outputs}, {predicted}")
-                    results.append([filename, outputs.tolist(), predicted.item()])
+        # Convert audio to feature encodings
+        audio_features = conv_audio_data(file_path, device, bundle)
 
-                predictions.append(predicted.item())
+        # Make predictions
+        with torch.no_grad():
+            print("predicting")
+            outputs = model(torch.tensor(audio_features).unsqueeze(0).to(device))
+            _, predicted = torch.max(outputs.data, 1)
+            print(f"Predicted for {filename}: {outputs}, {predicted}")
+            results.append([filename, outputs.tolist(), predicted.item()])
+
+        predictions.append(predicted.item())
 
     # Write results to CSV
     with open('prediction_results.csv', 'w', newline='') as file:
@@ -159,6 +159,18 @@ def process_directory(directory, model, device, bundle, true_labels):
     cm = confusion_matrix(true_labels, predictions)
     print("Confusion Matrix:\n", cm)
 
+    # Calculating the metrics
+    accuracy = accuracy_score(true_labels, predictions)
+    precision = precision_score(true_labels, predictions, zero_division=0)
+    recall = recall_score(true_labels, predictions, zero_division=0)
+    f1 = f1_score(true_labels, predictions, zero_division=0)
+
+    # Print the metrics
+    print(f"Accuracy: {accuracy:.4f}")
+    print(f"Precision: {precision:.4f}")
+    print(f"Recall: {recall:.4f}")
+    print(f"F1 Score: {f1:.4f}")
+
     # Visualize the confusion matrix
     plt.figure(figsize=(10, 7))
     sns.heatmap(cm, annot=True, fmt='g', cmap='viridis')
@@ -171,7 +183,21 @@ def process_directory(directory, model, device, bundle, true_labels):
     plt.savefig('confusion_matrix.png')
     print("Confusion matrix saved as 'confusion_matrix.png'.")
 
-model_path = "/content/drive/MyDrive/Ulima/Data/saves/DisfluencyNet_train_data_blk_quart.pth"
+def get_true_labels(audio_directory):
+    all_audio_files = []
+    true_labels = []
+
+    for root, dirs, files in os.walk(audio_directory):
+        for file in files:
+            if file.lower().endswith('.wav'):
+                file_path = os.path.join(root, file)
+                all_audio_files.append(file_path)
+                # Label as 1 if in "Block" subdirectory, else 0
+                true_labels.append(1 if "/WordRep/" in file_path else 0)
+
+    return all_audio_files, true_labels
+
+model_path = "/content/drive/MyDrive/Ulima/Data/saves/DisfluencyNet_train_data_wp_quart.pth"
 
 print("loading model")
 model = torch.load(model_path)
@@ -181,14 +207,19 @@ model.eval()
 # Directory containing your audio files
 audio_directory = "/content/drive/MyDrive/Ulima/Data/test_data"
 df = pd.read_csv('/content/drive/MyDrive/Ulima/Data/stuttering_test.csv')
-true_labels = df['Block'].tolist()
+
+# Generate true labels
+all_audio_files, true_labels = get_true_labels(audio_directory)
 
 # Debugging: Print out the first few labels and the length of the labels list
 print("First few true labels:", true_labels[:10])
 print("Number of true labels:", len(true_labels))
 
+# Debugging: Print out all true labels
+print("All true labels:", true_labels)
+
 # Continue with process_directory if true_labels is not empty
 if len(true_labels) > 0:
-    process_directory(audio_directory, model, device, bundle, true_labels)
+    process_directory(all_audio_files, model, device, bundle, true_labels)
 else:
     print("Error: True labels list is empty.")
