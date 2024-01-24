f7c46f96de2d2b33644fbc91062679496791e087
Marco Sobrevilla Cabezudo <msobrevillac@gmail.com>
2024-01-24 21:04:13 +0000

    refact(training): finish training

--- Disfluency detection/scripts/train.py
+++ Disfluency detection/scripts/train.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+import torch.nn as nn
 from dataset import load_dataset_from_path
 from utils import set_seed, __get_device__
 from utils import Wav2VecRepresentation
@@ -8,8 +10,69 @@ from sklearn.model_selection import train_test_split
 from utils import AudioDataset, get_dataloader
 from model import StutterNet
 
+
+def train(model, loader, optimizer, criterion):
+    model.train()
+    running_loss=0
+    correct=0
+    total=0
+    
+    for data in loader:
+        inputs, labels = data[0].to(device), data[1].to(device)
+        # forward pass
+        outputs=model(inputs)
+        loss=criterion(outputs,labels)
+        
+        # backward and optimise
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+        
+        # Compute Training Accuracy
+        _, predicted_labels = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += predicted_labels.eq(labels).sum().item()
+        
+    train_loss = running_loss/len(loader)
+    accu = 100.*correct/total
+    print(f'Train Loss: {train_loss:3f} | Accuracy: {accu:3f}')
+
+
+def evaluate(model, loader, criterion):
+    model.eval()
+    
+    running_loss=0
+    correct=0
+    total=0
+    
+    # These lists will store all the predictions and true labels
+    all_predictions = []
+    all_labels = []
+    
+    with torch.no_grad():
+        for data in loader:
+            features, labels = data[0].to(device), data[1].to(device)
+            outputs=model(features)
+            
+            _, predicted_valid = torch.max(outputs.data, 1)
+            all_predictions.extend(predicted_valid.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+            
+            loss= criterion(outputs,labels)
+            running_loss+=loss.item()
+            
+            total += labels.size(0)
+            correct += predicted_valid.eq(labels).sum().item()
+
+    eval_loss = running_loss/len(loader)
+    accu = 100.* correct/total
+    print(f'Validation Loss: {eval_loss:3f} | Accuracy: {accu:3f}')
+
 set_seed(42)
-wav2vec_rep = Wav2VecRepresentation(__get_device__())
+device = __get_device__()
+wav2vec_rep = Wav2VecRepresentation(device)
 
 subset = "train"
 
@@ -40,10 +103,12 @@ n_samples_dev = np.shape(x_dev)[0]
 n_samples_test = np.shape(x_test)[0]
 
 print('Number of samples to train = ', n_samples_train)
-print('Number of samples to validate = ', n_samples_valid)
+print('Number of samples to validate = ', n_samples_dev)
 print('Number of samples to test = ', n_samples_test)
 
 batch_size = 64
+num_epochs = 5
+learning_rate = 0.0001
 
 train_dataset = AudioDataset(x_train,y_train, n_samples_train)
 dev_dataset = AudioDataset(x_dev, y_dev, n_samples_dev)
@@ -53,4 +118,14 @@ train_loader = get_dataloader(train_dataset, batch_size, shuffle=True)
 dev_loader = get_dataloader(dev_dataset, batch_size)
 
 
+model = StutterNet(batch_size).to(device)
+print(model)
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
 
+epochs=num_epochs
+for epoch in range(1,epochs+1):
+    print('EPOCH {epoch} ...')
+    train(model, train_loader, optimizer, criterion)
+    evaluate(model, dev_loader, criterion)
